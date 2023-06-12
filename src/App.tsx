362cd138bf2aa791892a9ed03063d2752dcb387f
Hritik Bakshi <hritikbakshi@Hritiks-MacBook-Pro.local>
2023-06-12 11:43:49 +0530

    project setup

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+function App() {
+  return (<Router>{/* All routes are nested inside it */}</Router>);
+}
+
+export default App;
